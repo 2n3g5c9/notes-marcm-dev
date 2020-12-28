d76b47007124cae81a95368dc448bcab7c781a3c
Marc Molina <marc.m@outlook.com>
2020-12-28 20:50:51 +0000

    Fixes stored color access for Dark Mode

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,12 +3,12 @@
 import React from 'react'
 
 const getInitialTheme = _ => {
-  // if (typeof window !== 'undefined' && window.localStorage) {
-  //   const storedPrefs = window.localStorage.getItem('color-theme')
-  //   if (typeof storedPrefs === 'string') {
-  //     return storedPrefs
-  //   }
-  //
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme')
+    if (typeof storedPrefs === 'string') {
+      return storedPrefs
+    }
+  }
   //   const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
   //   if (userMedia.matches) {
   //     return 'dark'
