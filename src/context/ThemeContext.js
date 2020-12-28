0bd3bf12b587c9f1f2ba246feab1637540e55313
Marc Molina <marc.m@outlook.com>
2020-12-28 20:39:58 +0000

    Deactivates checking OS dark theme preferences

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,17 +3,17 @@
 import React from 'react'
 
 const getInitialTheme = _ => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const storedPrefs = window.localStorage.getItem('color-theme')
-    if (typeof storedPrefs === 'string') {
-      return storedPrefs
-    }
-
-    const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
-    if (userMedia.matches) {
-      return 'dark'
-    }
-  }
+  // if (typeof window !== 'undefined' && window.localStorage) {
+  //   const storedPrefs = window.localStorage.getItem('color-theme')
+  //   if (typeof storedPrefs === 'string') {
+  //     return storedPrefs
+  //   }
+  //
+  //   const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
+  //   if (userMedia.matches) {
+  //     return 'dark'
+  //   }
+  // }
 
   return 'light'
 }
