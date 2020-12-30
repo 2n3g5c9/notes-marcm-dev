4a443f2e0cae03ad3ce4eae862b670fef72034f7
Marc Molina <marc.m@outlook.com>
2020-12-30 13:19:50 +0000

    Adds first basic test with Jest and rewires Github Actions

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '^@components$': '<rootDir>/src/components',
+    '^@images$': '<rootDir>/src/images',
+  },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`<rootDir>/loadershim.js`],
+}
