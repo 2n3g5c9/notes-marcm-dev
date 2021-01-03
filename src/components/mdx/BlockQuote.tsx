3e39d6ce350fdb17598ad4b83e769c15a866a18d
Marc Molina <marc.m@outlook.com>
2021-01-03 12:50:13 +0000

    Makes blockquote h-scrollable if needed

--- src/components/mdx/BlockQuote.tsx
+++ src/components/mdx/BlockQuote.tsx
@@ -4,7 +4,7 @@ import { InferPropTypes } from '@shared/types'
 
 const BlockQuote = ({ children }: BlockQuoteProps): ReactElement => {
   return (
-    <div className="inline-block whitespace-pre my-2 px-4 py-3 border-l-2 sm:border-l-4 bg-secondary text-xs sm:text-sm text-secondary font-mono">
+    <div className="inline-block w-full sm:w-auto overflow-x-auto whitespace-pre my-2 px-4 py-3 border-l-2 sm:border-l-4 bg-secondary text-xs sm:text-sm text-secondary font-mono">
       <>{children}</>
     </div>
   )
