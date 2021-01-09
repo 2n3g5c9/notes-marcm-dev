07c2d2d04195b75436a088fd5c84de4b6297e6b2
Marc Molina <marc.m@outlook.com>
2021-01-09 19:12:36 +0000

    Adds confetti effect on Copy button

--- src/components/mdx/CopyButton.tsx
+++ src/components/mdx/CopyButton.tsx
@@ -2,9 +2,24 @@
 // @ts-nocheck
 import React, { ReactElement } from 'react'
 import PropTypes from 'prop-types'
+import Confetti from 'react-dom-confetti'
 
 import { copyToClipboard } from '@shared'
 import { InferPropTypes } from '@shared/types'
+
+const config = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: 70,
+  dragFriction: 0.12,
+  duration: 3000,
+  stagger: 3,
+  width: '10px',
+  height: '10px',
+  perspective: '500px',
+  colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+}
 
 const CopyButton = ({ text }: CopyButtonProps): ReactElement => {
   const [isCopied, setIsCopied] = React.useState(false)
@@ -15,9 +30,10 @@ const CopyButton = ({ text }: CopyButtonProps): ReactElement => {
       onClick={() => {
         copyToClipboard(text)
         setIsCopied(true)
-        setTimeout(() => setIsCopied(false), 3000)
+        setTimeout(() => setIsCopied(false), config.duration)
       }}
     >
+      <Confetti active={isCopied} config={config} />
       {isCopied ? '🎉 Copied!' : 'Copy'}
     </button>
   )
