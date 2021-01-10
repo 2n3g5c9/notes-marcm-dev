b14d5fda51c0a0830aa9aef150aba88a7ae54660
Marc Molina <marc.m@outlook.com>
2021-01-10 15:14:52 +0000

    Small fixes to text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,14 +9,12 @@ const HeroSection = ({ md }: HeroSectionProps): ReactElement => {
   return (
     <div className="bg-hero bg-secondary max-w-full mx-auto my-0 pt-0 px-4 pb-6">
       <div className="max-w-7xl mx-auto relative overflow-hidden">
-        <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-4xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-3xl lg:w-full lg:pb-26 xl:pb-32">
           <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-primary sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">{title}&nbsp;</span>
-                <span className="block text-accent xl:inline">
-                  #{hashtag}
-                </span>{' '}
+              <h1 className="text-4xl font-extrabold text-primary sm:text-5xl md:text-6xl">
+                <span className="inline">{title}&nbsp;</span>
+                <span className="text-accent inline">#{hashtag}</span>
               </h1>
               <p className="mt-3 text-base text-secondary sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 {subtitle}
