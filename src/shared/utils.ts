f8c6006492b43ac827c2b8e100384a84f1439db8
Marc Molina <marc.m@outlook.com>
2021-04-14 20:11:48 +0100

    Use clipboard access asynchronously

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -8,8 +8,12 @@ const categoryBg = (category: string, theme: string): string =>
 const categoryText = (category: string, theme: string): string =>
   `text-${categoryColors[category]}-${theme === 'dark' ? '50' : '700'}`
 
-const copyToClipboard = (str: string): void => {
-  navigator.clipboard.writeText(str)
+const copyToClipboard = async (str: string): Promise<void> => {
+  try {
+    await navigator.clipboard.writeText(str)
+  } catch (err) {
+    console.error('Failed to copy: ', err)
+  }
 }
 
 export { categoryBg, categoryText, copyToClipboard }
