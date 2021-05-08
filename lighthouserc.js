2b29b553e2106181092fe8969be131988f226b05
Marc Molina <marc.m@outlook.com>
2021-05-08 19:50:06 +0100

    Improves lighthouse-ci configuration

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,21 @@
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 5,
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'first-contentful-paint': [
+          'error',
+          { maxNumericValue: 2500, aggregationMethod: 'optimistic' },
+        ],
+        interactive: [
+          'max-potential-fid',
+          { maxNumericValue: 100, aggregationMethod: 'optimistic' },
+        ],
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
