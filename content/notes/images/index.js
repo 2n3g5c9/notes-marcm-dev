6dbe913a6a6afc68055fb0131ed8c5268ec35def
Marc Molina <marc.m@outlook.com>
2021-07-17 21:51:10 +0100

    Simplifies imports of SVGs in the first note

---
+++ content/notes/images/index.js
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import CDNSVG from './cdn.svg'
+import CISVG from './ci.svg'
+import CSSLibrarySVG from './css-library.svg'
+import DependencyManagementSVG from './dependency-management.svg'
+import HighLevelDesignSVG from './marcm-dev-hld.svg'
+import ProcessSVG from './process.svg'
+import StaticSiteGeneratorSVG from './static-site-generator.svg'
+import VCSSVG from './vcs.svg'
+
+export {
+  CDNSVG,
+  CISVG,
+  CSSLibrarySVG,
+  DependencyManagementSVG,
+  HighLevelDesignSVG,
+  ProcessSVG,
+  StaticSiteGeneratorSVG,
+  VCSSVG,
+}
